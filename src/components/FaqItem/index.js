b837afafc81e5d380880ba236ed48437ab9aa07b
Saravana0903 <saravanaadiraju@gmail.com>
2022-05-06 14:04:40 +0000

    doubt not clarifed

--- src/components/FaqItem/index.js
+++ src/components/FaqItem/index.js
@@ -4,37 +4,46 @@ import {Component} from 'react'
 import './index.css'
 
 class FaqItem extends Component {
-    const {details}=this.props 
-    
-    const {id,questionText,answerText} = this.details 
+  state = {
+    status: false,
+  }
 
-    state = {
-        status : false
-    }
-    
-    update = (){
-           this.setState(prev => ({status:!(prev.status)}))
-    }
-    
-    render(){
-        const {status} = this.state
-        return(
-    <li className = "faq-con">
-        <div className = "top-div">
-             <p className = "title">
-            {questionText}
-              </p>
-              <button className = "btn" onClick = {this.update}>
-                { status ? <img src = "https://assets.ccbp.in/frontend/react-js/faqs-minus-icon-img.png" className = "img-icon" alt = "minus" /> : <img src = "https://assets.ccbp.in/frontend/react-js/faqs-plus-icon-img.png" } className = "img-icon" alt = "plus" /> 
-                         }         
-             </button>
+  update = () => {
+    this.setState(prev => ({status: !prev.status}))
+  }
+
+  getPic1 = () => (
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/faqs-minus-icon-img.png"
+      className="img-icon"
+      alt="minus"
+    />
+  )
+
+  getPic2 = () => (
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/faqs-plus-icon-img.png"
+      className="img-icon"
+      alt="plus"
+    />
+  )
+
+  render() {
+    const {details} = this.props
+    const {id, questionText, answerText} = this.details
+    const {status} = this.state
+    return (
+      <li className="faq-con">
+        <div className="top-div">
+          <p className="title">{questionText}</p>
+          <button className="btn" onClick={this.update}>
+            {status ? this.getPic1() : this.getPic2()}
+          </button>
         </div>
-         {status && <hr/> }
-        <p className = "des">
-              {status  &&  answerText }
-        </p>
-    </li>
+        {status && <hr />}
+        <p className="des">{status && answerText}</p>
+      </li>
     )
-    }
+  }
 }
 export default FaqItem
